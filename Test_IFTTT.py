62b3aa23be0d226c405f580159e50e4033fb8fb5
JAMES-LAPTOP\James <jzampa@purdue.edu>
2019-10-27 18:37:00 -0400

    FUCK THIS CLASS

--- Test_IFTTT.py
+++ Test_IFTTT.py
@@ -3,6 +3,7 @@ from network import WLAN, STA_IF
 from ubinascii import hexlify
 import urequests
 import ujson
+from usocket import socket, getaddrinfo
 
 def connect_WiFi(ssid='NachoWifi', password='ICUPatnight'):
     wlan = WLAN(STA_IF)
@@ -21,12 +22,12 @@ def connect_WiFi(ssid='NachoWifi', password='ICUPatnight'):
     return wlan
 
 
-def http_get(url):
+def http_get(url, data):
     _, _, host, path = url.split('/', 3)
     addr = getaddrinfo(host, 80)[0][-1]
     s = socket()
     s.connect(addr)
-    s.send(bytes('GET /%s HTTP/1.0\r\nHost: %s\r\n\r\n' % (path, host), 'utf8'))
+    s.send(bytes('GET /%s HTTP/1.0\r\nHost: %s\r\nContent-Type: application/json\r\nContent-Length: %d\r\n\r\n%s\r\n\r\n' % (path, host, len(data), data), 'utf8'))
     while True:
         data = s.recv(100)
         if data:
@@ -36,7 +37,7 @@ def http_get(url):
     s.close()
     
     
-connect_WiFi()
+connect_WiFi('Is this the Krusty Krab?', 'tHiSiStHePaSsWoRd')
 
 #print('Installing UMQTT and HMAC packages...')
 #upip.install('micropython-umqtt.simple')
@@ -48,5 +49,6 @@ stuff['value1'] = '456'
 stuff['value2'] = '789'
 stuff['value3'] = '123'
 
-r = urequests.request('POST', 'https://maker.ifttt.com/trigger/UpdateSheet/with/key/diOQOLSzW1_Sh8OGpu4QgJ', data=ujson.dumps(stuff))
-
+#r = urequests.request('GET', 'https://maker.ifttt.com/trigger/UpdateSheet/with/key/diOQOLSzW1_Sh8OGpu4QgJ')
+http_get('https://maker.ifttt.com/trigger/UpdateSheet/with/key/diOQOLSzW1_Sh8OGpu4QgJ', ujson.dumps(stuff))
+#print(r.text)
